18cb8d046f2ce36d25bdb7d85e5974aadd88cb48
Ben Scheidelman <55633516+bscheidelman@users.noreply.github.com>
2023-08-25 13:11:43 -0500

    Update equity_bot.py

--- equity_bot.py
+++ equity_bot.py
@@ -2,10 +2,10 @@ from equity_calculator import calculate_equity_preflop, calculate_equity_flop, c
 import itertools, random
 
 def equity_bot_preflop(c1, c2):
-    equity = calculate_equity_preflop(c1, c2)
+    equity =  calculate_equity_preflop(c1, c2)
     if equity > 0.5:
         return 2.5
-    elif equity >= 0.3:
+    elif equity >= 0.4:
         return 0
     else:
         return -1
@@ -14,7 +14,7 @@ def equity_bot_preflop(c1, c2):
 def equity_bot_flop(c1, c2, b1, b2, b3):
     equity =  calculate_equity_flop(c1, c2, b1, b2, b3)
     if equity > 0.5:
-        return 2.5
+        return 5
     elif equity >= 0.3:
         return 0
     else:
@@ -24,7 +24,7 @@ def equity_bot_flop(c1, c2, b1, b2, b3):
 def equity_bot_turn(c1, c2, b1, b2, b3, b4):
     equity =  calculate_equity_turn(c1, c2, b1, b2, b3, b4)
     if equity > 0.5:
-        return 2.5
+        return 8
     elif equity >= 0.3:
         return 0
     else:
@@ -34,7 +34,7 @@ def equity_bot_turn(c1, c2, b1, b2, b3, b4):
 def equity_bot_river(c1, c2, b1, b2, b3, b4, b5):
     equity =  calculate_equity_river(c1, c2, b1, b2, b3, b4, b5)
     if equity > 0.5:
-        return 2.5
+        return 13
     elif equity >= 0.3:
         return 0
     else:
